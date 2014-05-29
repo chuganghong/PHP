fe9186c3e861a797c7ded52c938bff1772d699ba
chuganghong <chuganghong@gmail.com>
2014-05-29 09:37:46 +0800

    检测JS中字符串和数字相加的结果是什么
    add.php

---
+++ JS/add.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<script>
+/**
+ *检测JS中字符串和数字相加的结果是什么
+ *经测试，结果是字符串
+ */
+var str = 'Hello';
+var res = str + 2;
+alert(res);
+</script>
+
+</body>
+</html>
