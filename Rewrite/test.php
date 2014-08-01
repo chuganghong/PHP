01d7a8c0c6db92423c3489c08115a903e683519b
chuganghong <chuganghong@qq.com>
2014-08-01 08:53:37 +0800

    Apache url重写

---
+++ Rewrite/test.php
@@ -0,0 +1,4 @@
+<?php
+echo strtotime('2014-07-30');
+?>
+<p><a href="list/2007/04/5/onSubmit.jsp"><?php echo strtotime('Sunday'); ?></a></p>
--- Rewrite/view.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- *@filename:view.php
- *@description rewrite重写规则
- */
-//$url1 = 'http://localhost/Github/PHP/Rewrite/view.php?y=2009&m=9&t=using-rewrite-rules'
-//$url2 = 'http://localhost/Github/PHP/Rewrite/view.php/2009/9/using-rewrite-rules'
-var_dump($_GET);
-var_dump($_SERVER['REMOTE_ADDR']);
