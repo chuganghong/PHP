e686afeebd837454db57fb3e5ea6bdf63c545161
chuganghong <chuganghong@qq.com>
2014-08-05 21:35:37 +0800

    Apache重写

--- Rewrite/list.php
+++ Rewrite/list.php
@@ -1,2 +1,17 @@
 <?php
 var_dump($_GET);
+// var_dump($_SERVER['HTTP_REFERER']);
+// var_dump($_SERVER);
+$cur_url = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['REQUEST_URI'];
+// var_dump($cur_url);
+
+$suffix = get_suffix($cur_url);
+var_dump($suffix);
+function get_suffix($url)
+{
+	$pos = strrpos('/',$url);
+	$str = substr($url,$pos);
+	$arr = explode('.',$str);
+	$res = $arr[1];
+	return strpos('?',$res)?strstr($res,'?',true):$res;	
+}
--- Rewrite/test.php
+++ Rewrite/test.php
@@ -2,3 +2,4 @@
 echo strtotime('2014-07-30');
 ?>
 <p><a href="list/2007/04/5/onSubmit.jsp"><?php echo strtotime('Sunday'); ?></a></p>
+<p><a href="list/User/login.html"><?php echo strtotime('yesterday'); ?></a></p>
